0b21e3852c9691c7858743bcb6c21198ced8ae8d
= <=>
2018-03-12 15:03:15 -0300

    updating colors app and adding new model for auto save email when user login sucess

---
+++ app/src/main/java/com/jaimenejaim/android/animalcare/data/db/model/AutoSaveEmail.java
@@ -0,0 +1,27 @@
+package com.jaimenejaim.android.animalcare.data.db.model;
+
+/**
+ * Created by Jaime Nascimento Nejaim on 3/12/2018.
+ */
+
+public class AutoSaveEmail {
+
+    private long id;
+    private String email;
+
+
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
